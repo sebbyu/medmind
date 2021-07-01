6ee707fc6862ca3fd1a20d613c70c6373bf309c2
Sebbyu <you.sebastian.1.618@gmail.com>
2021-07-01 15:39:45 -0700

    navigation bar created

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,22 @@
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className="main">
+      <div></div>
+      <div className="nav_bar">
+        <div className="fas fa-dna"> MedMind</div>
+        <nav className="menu_bar">
+          <ul id="menu">
+            <li id="element"><span>About</span></li>
+            <li id="element"><span>Product</span></li>
+            <li id="element"><span>Resource</span></li>
+          </ul>
+        </nav>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Navbar;
